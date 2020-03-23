a9abca7374bd5c86a3af173418cfb0d078bff58e
Francois <frossouw@jhb.dvt.co.za>
2020-03-23 15:56:41 +0200

    Add tracklist service worker

---
+++ src/app/services/tracklist.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Tracklist } from '../models/tracklist';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TracklistService {
+  constructor(private http: HttpClient) { }
+  getTracklsit(query?: number): Observable<Tracklist> {
+    return this.http.get<Tracklist>(`${environment.apiUrl}${query}/top?limit=5`);
+  }
+}
