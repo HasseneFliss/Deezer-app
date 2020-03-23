079f4d9213a82414e8d7c1c467efdf455162b35e
Francois <frossouw@jhb.dvt.co.za>
2020-03-23 09:34:21 +0200

    Add artist service

---
+++ src/app/services/artist.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Artist } from '../models/artist';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtistService {
+
+  constructor(private http: HttpClient) { }
+  
+  getArtists(query?: string): Observable<Artist> {
+    return this.http.get<Artist>(`${environment.apiUrl}${query}`);
+  }
+}
