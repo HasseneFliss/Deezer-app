5fe82b5bbb60650812d9ccf15ef6447986e1a5df
Francois <frossouw@jhb.dvt.co.za>
2020-03-23 10:48:30 +0200

    Remodel artist

--- src/app/models/artist.ts
+++ src/app/models/artist.ts
@@ -6,12 +6,12 @@ export class Artist {
     link: string;
     share: string;
     picture: string;
-    pictureSmall: string;
-    pictureMedium: string;
-    pictureBig: string;
-    pictureXl: string;
-    nbAlbum: number;
-    nbFan: number;
+    picture_small: string;
+    picture_medium: string;
+    picture_big: string;
+    picture_xl: string;
+    nb_album: number;
+    nb_fan: number;
     radio: boolean;
     tracklist: string;
     type: string;
