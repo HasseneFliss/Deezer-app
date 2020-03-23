1892f075f20fbf94b9339d9c0434219b01d31f8a
Francois <frossouw@jhb.dvt.co.za>
2020-03-23 15:52:14 +0200

    Add model track

---
+++ src/app/models/track.ts
@@ -0,0 +1,10 @@
+export class Track {
+    id: number;
+    readable: boolean;
+    title: string;
+    title_short: string;
+    title_version: string;
+    unseen: boolean;
+    duration: number;
+
+}
