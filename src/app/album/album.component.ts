163414d40a951cb413c448a9cd1126c5409f0815
Francois <frossouw@jhb.dvt.co.za>
2020-03-24 14:48:26 +0200

    Create album component HTML
    Html for component created
    Basic css apply
    Routing configured

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Artist } from '../models/artist';
+import { AlbumService } from '../services/album.service';
+import { Albums } from '../models/albums';
+import { Album } from '../models/album';
 
 @Component({
   selector: 'app-album',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  albumArtist: Artist;
+  albums: Albums;
+  album_list: Album[] = new Array();
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit(): void {
+    this.albumService.getAlbums(this.albumArtist.id)
+      .subscribe((x) => {
+        if (!('error' in x)) {
+          this.albums = x;
+          this.album_list = this.albums.data;
+        }
+      });
   }
-
 }
