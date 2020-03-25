8b01f2b54605d66e160a1d978fbb4d8a0a08b0ac
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-03-25 10:55:34 +0200

    CICD Lint Error fix (" ") to (' ')

--- src/app/pipes/fan-formatter.pipe.ts
+++ src/app/pipes/fan-formatter.pipe.ts
@@ -7,8 +7,8 @@ export class FanFormatterPipe implements PipeTransform {
 
   transform(value: number): string {
     value = Math.floor(value / 1000);
-    const num_parts = value.toString().split(".");
-    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return `${num_parts.join(".")} k Fans`;
+    const num_parts = value.toString().split('.');
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return `${num_parts.join('.')} k Fans`;
   }
 }
