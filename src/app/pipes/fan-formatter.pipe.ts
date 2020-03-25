27d3a471c5b973f693f19cca9a1469c6d1b5f726
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-03-25 10:51:05 +0200

    Fix display of numbers to include "," in 1000's

--- src/app/pipes/fan-formatter.pipe.ts
+++ src/app/pipes/fan-formatter.pipe.ts
@@ -7,6 +7,8 @@ export class FanFormatterPipe implements PipeTransform {
 
   transform(value: number): string {
     value = Math.floor(value / 1000);
-    return `${value} k Fans`;
+    const num_parts = value.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return `${num_parts.join(".")} k Fans`;
   }
 }
