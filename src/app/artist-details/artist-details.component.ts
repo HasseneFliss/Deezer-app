2c4217818588ad67750b81ac7210d573bf696729
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-03-30 09:08:43 +0200

    Code refactor on issue #22
    Code was reverted due to branch error
    Code refactor
    Console log removed from artist-details.component.ts

--- src/app/artist-details/artist-details.component.ts
+++ src/app/artist-details/artist-details.component.ts
@@ -20,7 +20,6 @@ export class ArtistDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.activatedroute.paramMap.subscribe(params => {
       this.artistId = Number(params.get('id'));
-      console.log(this.artistId);
 
       this.artistService.getArtist(this.artistId)
         .subscribe((x) => {
