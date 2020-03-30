d3d1e82bba82bc92ca641fea4c63d090aa137210
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-03-30 09:44:23 +0200

    Code refactor on issue #19
    Artists class removed
    Code refactored to use an array of artist[]

--- src/app/artists/artists.component.ts
+++ src/app/artists/artists.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ArtistService } from '../services/artist.service';
-import { Artists } from '../models/artists';
 import { Artist } from '../models/artist';
 
 @Component({
@@ -12,23 +11,19 @@ export class ArtistsComponent implements OnInit {
 
   artists: Artist[] = new Array();
   artist: Artist;
-  artistList: Artists;
 
   constructor(private artistService: ArtistService) { }
 
   ngOnInit(): void {
-    this.artistList = new Artists();
     for (let artistNo = 2; artistNo <= 22; artistNo++) {
       this.artistService.getArtist(artistNo)
         .subscribe((x) => {
           if (!('error' in x)) {
-            this.artist = x;
-            this.artists.push(this.artist);
+            this.artists.push(x);
           }
 
         });
     }
-    this.artistList.artists = this.artists;
   }
 
 }
--- src/app/models/artists.ts
+++
@@ -1,7 +0,0 @@
-import { Artist } from './artist';
-
-export class Artists {
-    constructor() {}
-
-    artists: Artist[] = new Array();
-}
